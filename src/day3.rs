9ee7b8f58cb6b2daf9764cb2d76ead0a2c59cdfc
Jade Misenas <cjmisenas@gmail.com>
2021-01-03 23:25:41 -0800

    Cleanup day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::File;
@@ -6,19 +7,30 @@ use std::path::Path;
 
 pub fn main() {
     let lines = read_lines_as_str("./day3.input");
-    let wire_paths = lines
+    let wires = lines
         .iter()
         .map(|l| l.split(",").map(|p| p.to_string()).collect::<Vec<String>>())
         .collect::<Vec<Vec<String>>>();
-    let answer1 = solve1(&wire_paths);
-    let answer2 = solve2(&wire_paths);
+    let wire_paths = parse_wires(&wires);
+    let intersections = get_wire_intersections(&wire_paths);
+    let answer1 = solve1(&intersections);
+    let answer2 = solve2(&intersections, &wire_paths);
     println!("Day 3 answers");
     println!("Answer 1 {}", answer1);
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(wires: &Vec<Vec<String>>) -> u32 {
+fn get_wire_intersections(wire_paths: &Vec<Vec<(i32, i32)>>) -> Vec<(i32, i32)> {
     let mut intersections: Vec<(i32, i32)> = Vec::new();
+    for (i, wire) in wire_paths.iter().enumerate() {
+        for other_wire in wire_paths.iter().skip(i + 1) {
+            intersections = [intersections, get_intersections(&wire, &other_wire)].concat();
+        }
+    }
+    intersections
+}
+
+fn parse_wires(wires: &Vec<Vec<String>>) -> Vec<Vec<(i32, i32)>> {
     let mut wire_paths: Vec<Vec<(i32, i32)>> = Vec::new();
     for wire in wires {
         // Start at origin
@@ -34,12 +46,10 @@ fn solve1(wires: &Vec<Vec<String>>) -> u32 {
         }
         wire_paths.push(curr_wire);
     }
-    for (i, wire) in wire_paths.iter().enumerate() {
-        for other_wire in wire_paths.iter().skip(i + 1) {
-            let mut found = get_intersections(&wire, &other_wire);
-            intersections = [intersections, found].concat();
-        }
-    }
+    wire_paths
+}
+
+fn solve1(intersections: &Vec<(i32, i32)>) -> u32 {
     let min = intersections
         .iter()
         .filter(|i| !(i.0 == 0 && i.1 == 0))
@@ -52,7 +62,6 @@ fn get_intersections(wire_a: &Vec<(i32, i32)>, wire_b: &Vec<(i32, i32)>) -> Vec<
     let mut intersections: Vec<(i32, i32)> = Vec::new();
     for (i, path_a) in wire_a.iter().enumerate().skip(1) {
         for (j, path_b) in wire_b.iter().enumerate().skip(1) {
-            // Sort the coordinates first so left and bottom-most of end of wire is first.
             let start_a = wire_a[i - 1];
             let start_b = wire_b[j - 1];
             if let Some(intersection) = check_intersects((start_a, *path_a), (start_b, *path_b)) {
@@ -68,25 +77,21 @@ fn check_intersects(
     wire_b: ((i32, i32), (i32, i32)),
 ) -> Option<(i32, i32)> {
     // wire_b is horizontal and wire_a is vertical
-    if wire_a.0 .0 == wire_a.1 .0 && wire_b.0 .1 == wire_b.1 .1 {
-        if std::cmp::min(wire_b.0 .0, wire_b.1 .0) <= wire_a.0 .0
-            && std::cmp::max(wire_b.0 .0, wire_b.1 .0) >= wire_a.0 .0
-            && std::cmp::min(wire_a.0 .1, wire_a.1 .1) <= wire_b.0 .1
-            && std::cmp::max(wire_a.0 .1, wire_a.1 .1) >= wire_b.0 .1
-        {
-            return Some((wire_a.0 .0, wire_b.0 .1));
-        }
-    // wire_b is vertical and wire_a is horizontal
-    } else if wire_b.0 .0 == wire_b.1 .0 && wire_a.0 .1 == wire_a.1 .1 {
-        if std::cmp::min(wire_a.0 .0, wire_a.1 .0) <= wire_b.0 .0
-            && std::cmp::max(wire_a.0 .0, wire_a.1 .0) >= wire_b.0 .0
-            && std::cmp::min(wire_b.0 .1, wire_b.1 .1) <= wire_a.0 .1
-            && std::cmp::max(wire_b.0 .1, wire_b.1 .1) >= wire_a.0 .1
-        {
-            return Some((wire_b.0 .0, wire_a.0 .1));
-        }
+    if min(wire_b.0 .0, wire_b.1 .0) <= wire_a.0 .0
+        && max(wire_b.0 .0, wire_b.1 .0) >= wire_a.0 .0
+        && min(wire_a.0 .1, wire_a.1 .1) <= wire_b.0 .1
+        && max(wire_a.0 .1, wire_a.1 .1) >= wire_b.0 .1
+    {
+        Some((wire_a.0 .0, wire_b.0 .1))
+    } else if min(wire_a.0 .0, wire_a.1 .0) <= wire_b.0 .0
+        && max(wire_a.0 .0, wire_a.1 .0) >= wire_b.0 .0
+        && min(wire_b.0 .1, wire_b.1 .1) <= wire_a.0 .1
+        && max(wire_b.0 .1, wire_b.1 .1) >= wire_a.0 .1
+    {
+        Some((wire_b.0 .0, wire_a.0 .1))
+    } else {
+        None
     }
-    None
 }
 
 fn parse_dir(mut dir: String) -> (i32, i32) {
@@ -100,30 +105,7 @@ fn parse_dir(mut dir: String) -> (i32, i32) {
     }
 }
 
-fn solve2(wires: &Vec<Vec<String>>) -> u32 {
-    let mut intersections: Vec<(i32, i32)> = Vec::new();
-    let mut wire_paths: Vec<Vec<(i32, i32)>> = Vec::new();
-    for wire in wires {
-        // Start at origin
-        let mut x = 0;
-        let mut y = 0;
-        let mut curr_wire: Vec<(i32, i32)> = Vec::new();
-        curr_wire.push((x, y));
-        for path in wire {
-            let (new_x, new_y) = parse_dir(path.to_string());
-            x += new_x;
-            y += new_y;
-            curr_wire.push((x, y));
-        }
-        wire_paths.push(curr_wire);
-    }
-    for (i, wire) in wire_paths.iter().enumerate() {
-        for other_wire in wire_paths.iter().skip(i + 1) {
-            let mut found = get_intersections(&wire, &other_wire);
-            intersections = [intersections, found].concat();
-        }
-    }
-    println!("{:?}", intersections);
+fn solve2(intersections: &Vec<(i32, i32)>, wire_paths: &Vec<Vec<(i32, i32)>>) -> u32 {
     intersections
         .iter()
         .filter(|i| !(i.0 == 0 && i.1 == 0))
@@ -136,15 +118,7 @@ fn get_steps(point: (i32, i32), wire: &Vec<(i32, i32)>) -> u32 {
     let mut steps = 0;
     for (i, end_path) in wire.iter().enumerate().skip(1) {
         let start_path = wire[i - 1];
-        if (point.0 == end_path.0
-            && point.0 == start_path.0
-            && point.1 <= std::cmp::max(end_path.1, start_path.1)
-            && point.1 >= std::cmp::min(end_path.1, start_path.1))
-            || (point.1 == end_path.1
-                && point.1 == start_path.1
-                && point.0 <= std::cmp::max(end_path.0, start_path.0)
-                && point.0 >= std::cmp::min(end_path.0, start_path.0))
-        {
+        if is_between_line(point, start_path, *end_path) {
             steps += (start_path.0 - point.0).abs();
             steps += (start_path.1 - point.1).abs();
             break;
@@ -156,6 +130,17 @@ fn get_steps(point: (i32, i32), wire: &Vec<(i32, i32)>) -> u32 {
     steps as u32
 }
 
+fn is_between_line(point: (i32, i32), start: (i32, i32), end: (i32, i32)) -> bool {
+    (point.0 == end.0
+        && point.0 == start.0
+        && point.1 <= max(end.1, start.1)
+        && point.1 >= min(end.1, start.1))
+        || (point.1 == end.1
+            && point.1 == start.1
+            && point.0 <= max(end.0, start.0)
+            && point.0 >= min(end.0, start.0))
+}
+
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
 where
     P: AsRef<Path>,
@@ -164,16 +149,5 @@ where
     let buf = io::BufReader::new(file);
     buf.lines()
         .map(|l| l.expect("Could not parse line"))
-        .collect()
-}
-
-fn read_lines_as_int<P>(filename: P) -> Vec<u32>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename).expect("no such file");
-    let buf = io::BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line").parse::<u32>().unwrap())
         .collect()
 }
