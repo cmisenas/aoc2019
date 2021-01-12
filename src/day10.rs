46591a032e6628c30a67dea4c405ec83242b8a99
Jade Misenas <cjmisenas@gmail.com>
2021-01-11 16:35:44 -0800

    Day 10 part 2 done

--- src/day10.rs
+++ src/day10.rs
@@ -24,30 +24,39 @@ pub fn main() {
             }
         }
     }
-    let answer1 = solve1(&asteroids);
-    let answer2 = solve2(&lines);
+    let best_asteroid = get_best_asteroid(&asteroids);
+    let answer1 = best_asteroid.1.len();
+    let answer2 = solve2(*best_asteroid.0, best_asteroid.1, &asteroids);
     println!("Day 10 answers");
     println!("Answer 1 {}", answer1);
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(asteroids: &Vec<(usize, usize)>) -> usize {
+fn get_best_asteroid(
+    asteroids: &Vec<(usize, usize)>,
+) -> (&(usize, usize), HashMap<String, (usize, usize)>) {
     let scores = asteroids
         .iter()
         .map(|asteroid| {
             (
                 asteroid,
-                calc_asteroid_score(asteroid.0, asteroid.1, asteroids),
+                get_asteroids_los(asteroid.0, asteroid.1, asteroids),
             )
         })
-        .collect::<Vec<(&(usize, usize), usize)>>();
+        .collect::<Vec<(&(usize, usize), HashMap<String, (usize, usize)>)>>();
 
-    let max_score = scores.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap();
-    println!("{:?}", max_score);
-    max_score.1
+    let best_asteroid = scores
+        .iter()
+        .max_by(|a, b| a.1.len().cmp(&b.1.len()))
+        .unwrap();
+    best_asteroid.clone()
 }
 
-fn calc_asteroid_score(x: usize, y: usize, asteroids: &Vec<(usize, usize)>) -> usize {
+fn get_asteroids_los(
+    x: usize,
+    y: usize,
+    asteroids: &Vec<(usize, usize)>,
+) -> HashMap<String, (usize, usize)> {
     let mut total_in_los: HashMap<String, (usize, usize)> = HashMap::new();
     for asteroid in asteroids.iter() {
         if asteroid.0 == x && asteroid.1 == y {
@@ -69,7 +78,7 @@ fn calc_asteroid_score(x: usize, y: usize, asteroids: &Vec<(usize, usize)>) -> u
             total_in_los.insert(ratio_distance, *asteroid);
         }
     }
-    total_in_los.len()
+    total_in_los
 }
 
 fn simplify_fraction(mut numerator: i16, mut denominator: i16) -> String {
@@ -109,8 +118,69 @@ fn gcd(mut num: u16, mut den: u16) -> u16 {
     }
 }
 
-fn solve2(lines: &[String]) -> u8 {
-    0
+fn solve2(
+    best_asteroid: (usize, usize),
+    asteroid_los: HashMap<String, (usize, usize)>,
+    asteroids: &Vec<(usize, usize)>,
+) -> usize {
+    use std::cmp::Ordering;
+    use std::f64::{INFINITY, NEG_INFINITY};
+    let mut sorted_asteroids = asteroid_los
+        .iter()
+        .map(|(distance, asteroid)| (distance.to_string(), asteroid.clone()))
+        .collect::<Vec<(String, (usize, usize))>>();
+
+    sorted_asteroids.sort_by(|(distance_a, _), (distance_b, _)| {
+        let distance_a_ = distance_a
+            .split("/")
+            .map(|a| a.parse::<i8>().unwrap())
+            .collect::<Vec<i8>>();
+        let distance_b_ = distance_b
+            .split("/")
+            .map(|a| a.parse::<i8>().unwrap())
+            .collect::<Vec<i8>>();
+        let a_quadrant = get_quadrant(distance_a_[1], distance_a_[0]);
+        let b_quadrant = get_quadrant(distance_b_[1], distance_b_[0]);
+        if a_quadrant == b_quadrant {
+            let calc_distance_a = distance_a_[0] as f64 / distance_a_[1] as f64;
+            let calc_distance_b = distance_b_[0] as f64 / distance_b_[1] as f64;
+            calc_distance_a.partial_cmp(&calc_distance_b).unwrap()
+        } else {
+            a_quadrant.cmp(&b_quadrant)
+        }
+    });
+    let asteroid_200 = sorted_asteroids[199].1;
+    asteroid_200.0 * 100 + asteroid_200.1
+}
+
+fn get_quadrant(x: i8, y: i8) -> u8 {
+    // N = 0
+    // NE = 1
+    // E = 2
+    // SE = 3
+    // S = 4
+    // SW = 5
+    // W = 6
+    // NW = 7
+    if x == 0 && y > 0 {
+        0
+    } else if x < 0 && y > 0 {
+        1
+    } else if y == 0 && x < 0 {
+        2
+    } else if x < 0 && y < 0 {
+        3
+    } else if x == 0 && y < 0 {
+        4
+    } else if x > 0 && y < 0 {
+        5
+    } else if y == 0 && x > 0 {
+        6
+    } else if y > 0 && x > 0 {
+        7
+    } else {
+        panic!("Quadrant not found");
+    }
 }
 
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
