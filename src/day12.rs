001767e1cc4b1e6ae92920d394e78f912376d9bb
Jade Misenas <cjmisenas@gmail.com>
2021-01-12 21:16:11 -0800

    Day 12 part 2 done

--- src/day12.rs
+++ src/day12.rs
@@ -14,7 +14,7 @@ pub fn main() {
         .map(|line| parse_moon(line.to_string()))
         .collect::<Vec<(i16, i16, i16)>>();
     let answer1 = solve1(moons.clone());
-    let answer2 = solve2(&moons);
+    let answer2 = solve2(moons.clone());
     println!("Day 12 answers");
     println!("Answer 1 {}", answer1);
     println!("Answer 2 {}", answer2);
@@ -64,8 +64,87 @@ fn calculate_total_energy(moons: &Vec<(i16, i16, i16)>, velocities: &Vec<(i16, i
     })
 }
 
-fn solve2(moons: &Vec<(i16, i16, i16)>) -> u32 {
-    0
+fn solve2(moons: Vec<(i16, i16, i16)>) -> usize {
+    use std::cmp::max;
+
+    let x = subset_cycle(0, moons.clone());
+    let y = subset_cycle(1, moons.clone());
+    let z = subset_cycle(2, moons.clone());
+    let inc = max(max(x, y), z);
+    let mut loops: usize = 0;
+    loop {
+        loops += inc;
+        if loops % x == 0 && loops % y == 0 && loops % z == 0 {
+            break;
+        }
+    }
+    loops
+}
+
+fn subset_cycle(dimension: u8, mut moons: Vec<(i16, i16, i16)>) -> usize {
+    let mut moon_velocity = vec![0; moons.len()];
+    let mut loops = 0;
+    let mut initial_config = String::new();
+    for moon in moons.iter() {
+        if dimension == 0 {
+            initial_config.push_str(&moon.0.to_string());
+        } else if dimension == 1 {
+            initial_config.push_str(&moon.1.to_string());
+        } else {
+            initial_config.push_str(&moon.2.to_string());
+        }
+        initial_config.push_str("0");
+    }
+    loop {
+        loops += 1;
+        let mut str_config = String::new();
+        for (a, moon_a) in moons.iter().enumerate() {
+            for (b, moon_b) in moons.iter().enumerate().skip(a + 1) {
+                if dimension == 0 {
+                    if moon_a.0 > moon_b.0 {
+                        moon_velocity[b] += 1;
+                        moon_velocity[a] -= 1;
+                    } else if moon_a.0 < moon_b.0 {
+                        moon_velocity[a] += 1;
+                        moon_velocity[b] -= 1;
+                    }
+                } else if dimension == 1 {
+                    if moon_a.1 > moon_b.1 {
+                        moon_velocity[b] += 1;
+                        moon_velocity[a] -= 1;
+                    } else if moon_a.1 < moon_b.1 {
+                        moon_velocity[a] += 1;
+                        moon_velocity[b] -= 1;
+                    }
+                } else {
+                    if moon_a.2 > moon_b.2 {
+                        moon_velocity[b] += 1;
+                        moon_velocity[a] -= 1;
+                    } else if moon_a.2 < moon_b.2 {
+                        moon_velocity[a] += 1;
+                        moon_velocity[b] -= 1;
+                    }
+                }
+            }
+        }
+        for (i, moon) in moons.iter_mut().enumerate() {
+            if dimension == 0 {
+                moon.0 += moon_velocity[i];
+                str_config.push_str(&moon.0.to_string());
+            } else if dimension == 1 {
+                moon.1 += moon_velocity[i];
+                str_config.push_str(&moon.1.to_string());
+            } else {
+                moon.2 += moon_velocity[i];
+                str_config.push_str(&moon.2.to_string());
+            }
+            str_config.push_str(&moon_velocity[i].to_string());
+        }
+        if initial_config == str_config {
+            break;
+        }
+    }
+    loops
 }
 
 fn parse_moon(moon: String) -> (i16, i16, i16) {
