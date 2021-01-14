fe639713bd9b4c03a255b95a3b24221f2bec5760
Jade Misenas <cjmisenas@gmail.com>
2021-01-14 12:07:32 -0800

    Fix visualization

--- src/day13.rs
+++ src/day13.rs
@@ -45,6 +45,18 @@ fn solve2(mut objects: Vec<HashSet<(usize, usize)>>, mut program: Vec<i64>) -> i
     let mut score = 0;
     let mut paddle_pos = objects[3].iter().nth(0).unwrap().clone();
     let mut ball_pos = objects[4].iter().nth(0).unwrap().clone();
+    let height = objects[1]
+        .iter()
+        .max_by(|(_, y1), (_, y2)| y1.cmp(y2))
+        .unwrap()
+        .1 as usize
+        + 1;
+    let width = objects[1]
+        .iter()
+        .max_by(|(x1, _), (x2, _)| x1.cmp(x2))
+        .unwrap()
+        .0 as usize
+        + 1;
     while !computer.has_halted {
         let x = computer.run_program();
         let y = computer.run_program();
@@ -60,9 +72,11 @@ fn solve2(mut objects: Vec<HashSet<(usize, usize)>>, mut program: Vec<i64>) -> i
             } else if val == 4 {
                 objects[4].clear();
                 objects[4].insert(new_pos);
+                ball_pos = new_pos;
+            }
+            if val != 2 {
                 // Remove block tile if any
                 objects[2].remove(&new_pos);
-                ball_pos = new_pos;
             }
 
             if paddle_pos.0 < ball_pos.0 {
@@ -73,26 +87,15 @@ fn solve2(mut objects: Vec<HashSet<(usize, usize)>>, mut program: Vec<i64>) -> i
                 computer.input = 0;
             }
         }
-        draw_game(&objects, score);
+        draw_game(width, height, &objects, score);
     }
+    draw_game(width, height, &objects, score);
     score
 }
 
-fn draw_game(objects: &Vec<HashSet<(usize, usize)>>, score: i64) {
+fn draw_game(width: usize, height: usize, objects: &Vec<HashSet<(usize, usize)>>, score: i64) {
     use std::{thread, time};
-    let height = objects[1]
-        .iter()
-        .max_by(|(_, y1), (_, y2)| y1.cmp(y2))
-        .unwrap()
-        .1
-        + 1;
-    let width = objects[1]
-        .iter()
-        .max_by(|(x1, _), (x2, _)| x1.cmp(x2))
-        .unwrap()
-        .0
-        + 1;
-    let mut game_state = vec![vec![' '; width as usize]; height as usize];
+    let mut game_state = vec![vec![' '; width]; height];
     for (tile_type, obj) in objects.iter().enumerate() {
         for pos in obj {
             match tile_type {
@@ -105,6 +108,9 @@ fn draw_game(objects: &Vec<HashSet<(usize, usize)>>, score: i64) {
             }
         }
     }
+
+    // Clear screen and reset
+    print!("\x1B[2J\x1B[1;1H");
     println!(
         "{}\nScore: {}",
         game_state
@@ -114,11 +120,8 @@ fn draw_game(objects: &Vec<HashSet<(usize, usize)>>, score: i64) {
             .join("\n"),
         score
     );
-    let five_millis = time::Duration::from_millis(5);
-    thread::sleep(five_millis);
-
-    // Clear screen and reset
-    print!("\x1B[2J\x1B[1;1H");
+    let three_millis = time::Duration::from_millis(3);
+    thread::sleep(three_millis);
 }
 
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
